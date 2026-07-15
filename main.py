d170da33812532f9dcf57fa211b4d0a8fa7d8b71
ykamal310 <ykamal@umich.edu>
2026-07-15 04:48:34 -0400

    create main py for app entry

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+
+from PyQt6.QtWidgets import QApplication
+
+from app.authentication import create_account
+from app.database import create_tables
+from app.login_window import LoginWindow
+
+
+create_tables()
+
+#create_account("admin", "Admin123!", "Administrator")
+
+app = QApplication(sys.argv)
+
+window = LoginWindow()
+window.show()
+
+sys.exit(app.exec())
